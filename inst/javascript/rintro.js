5bbebc25fbb194e572214e160c951e249d89fee3
carlganz <carlganz@gmail.com>
2018-04-24 09:39:05 -0700

    create rintrojs namespace and add callbacks there

--- inst/javascript/rintro.js
+++ inst/javascript/rintro.js
@@ -36,3 +36,22 @@ Shiny.addCustomMessageHandler('hintjs',function(options) {
         options.events.hasOwnProperty("onhintclose") && eval(options.events.onhintclose[0]);
         }).addHints();
 });
+
+rintrojs = function() {
+  return {
+    callback : {
+      switchTabs : function(targetElement) {
+        // get nodelist of tab panes 
+        var tabpanes = document.querySelectorAll("div.tab-pane");
+        // iterate over the list of tab panes and click their corresponding link
+        // element if the node contains the upcoming target element 
+        for (i = 0; i < tabpanes.length; i++) {
+          if (tabpanes[i].contains(targetElement)) {
+            var selector = "a[href = \"#" + tabpanes[i].id + "\"]";
+            document.querySelector(selector).click();
+          }
+        }
+      }
+    }
+  };
+}();
