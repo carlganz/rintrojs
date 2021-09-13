c77640dcf7740978672d54daaa8db3a83749b53f
carlganz <carlganz@gmail.com>
2021-09-13 11:38:33 -0700

    update selector to require data-value attribute in case users use tab-pane in unexpected ways

--- inst/javascript/rintro.js
+++ inst/javascript/rintro.js
@@ -45,7 +45,7 @@ rintrojs = function() {
     callback : {
       switchTabs : function(targetElement) {
         // get nodelist of tab panes 
-        var tabpanes = document.querySelectorAll("div.tab-pane");
+        var tabpanes = document.querySelectorAll("div.tab-pane[data-value]");
         // iterate over the list of tab panes and click their corresponding link
         // element if the node contains the upcoming target element 
         for (i = 0; i < tabpanes.length; i++) {
